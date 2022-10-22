548f8f021f31fcf52a2c62618ea8a75447e19085
Handikha Jul <handikha@gmail.com>
2022-10-22 09:08:39 +0700

    farikh design test

--- dist/js/script.js
+++ dist/js/script.js
@@ -3,11 +3,20 @@ const navButtonSpan = document.querySelector("#nav-button > span");
 const nav = document.querySelector("#nav");
 const brandSm = document.querySelector("#brand-sm");
 const content = document.querySelector("#content");
+const rightNav = document.querySelector("#right-nav");
 
 navButton.addEventListener("click", () => {
   navButton.classList.toggle("nav-active");
   nav.classList.toggle("-translate-x-full");
   brandSm.classList.toggle("-translate-x-full");
+  rightNav.classList.toggle("-translate-y-[400%]");
+});
+
+rightNav.addEventListener("click", () => {
+  navButton.classList.toggle("nav-active");
+  nav.classList.toggle("-translate-x-full");
+  brandSm.classList.toggle("-translate-x-full");
+  rightNav.classList.toggle("-translate-y-[400%]");
 });
 
 // SUBMENU
